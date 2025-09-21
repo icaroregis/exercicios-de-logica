e03bb18e8ebc7e1179b45fa9dcb00d060aa0b20f
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2025-09-21 15:38:50 -0300

    feat: corrigir a função obterMensagemDaUltimaTemperatura e adicionar a função obterUltimaNota

--- ultimo-item-da-matriz.js
+++ ultimo-item-da-matriz.js
@@ -1,7 +1,11 @@
 function obterMensagemDaUltimaTemperatura(temperaturas) {
-  return `A última temperatura está na posição ${temperaturas[temperaturas.length - 1]}`;
+  return `A última temperatura está na posição ${temperaturas.indexOf(temperaturas[temperaturas.length - 1])}`;
 }
 
 // Não modifique as linhas abaixo
 console.log(obterMensagemDaUltimaTemperatura([-5, 12, 15])); // "A última temperatura está na posição 2"
 console.log(obterMensagemDaUltimaTemperatura([13, 10])); // "A última temperatura está na posição 1"
+
+function obterUltimaNota(notas) {
+  return notas.indexOf(notas[notas.length - 1]);
+}
