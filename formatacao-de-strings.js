02493c72ce35961daac8c99070b5d9a882c69413
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2025-09-28 16:32:42 -0300

    feat: adicionar exemplo de uso de template literals e quebra de linha em mensagens

---
+++ formatacao-de-strings.js
@@ -0,0 +1,8 @@
+//Quebra de linhas com template literals (``)
+const mensagem = `Olá, Mundo!
+Seja bem-vindo ao JavaScript.`;
+// quebra de linha
+const mensagem2 = 'Olá, Mundo!\nSeja bem-vindo ao JavaScript.';
+//escapar caractere especial (\')
+const mensagem4 = 'Você prefere um copo d\'água ou um guaraná "caçulinha"?';
+console.log(mensagem4);
