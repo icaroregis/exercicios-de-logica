a5a3c58c27db582da8500f98b41ce9416292664e
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2025-10-05 13:52:32 -0300

    feat: adicionar a classe Receita com métodos para verificar calorias e exemplos de uso

--- classes/metodos-de-instancia.js
+++ classes/metodos-de-instancia.js
@@ -41,3 +41,34 @@ let usuario = new Usuario('Daniela', 'Queiroz');
 console.log(usuario.obterNomeCompleto());
 console.log(usuario.podeVotar());
 console.log(usuario.jaVotou());
+
+// Definição da classe
+class Receita {
+  constructor(nome, calorias) {
+    this.nome = nome;
+    this.calorias = calorias;
+  }
+
+  ehBaixaCaloria() {
+    if (this.calorias <= 400) {
+      return true;
+    }
+    return false;
+  }
+
+  ehAltaCaloria() {
+    if (this.calorias >= 600) {
+      return true;
+    }
+    return false;
+  }
+}
+
+// Uso da classe
+let macarrao = new Receita('Macarrão', 700);
+console.log(macarrao.ehBaixaCaloria());
+console.log(macarrao.ehAltaCaloria());
+
+let salada = new Receita('Salada', 350);
+console.log(salada.ehBaixaCaloria());
+console.log(salada.ehAltaCaloria());
