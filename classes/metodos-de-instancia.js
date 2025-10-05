421f07d528e2db30c0046425fec26b6f3462709c
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2025-10-05 13:35:50 -0300

    feat: adicionar a classe Usuario com métodos para obter nome completo e verificar status de votação

--- classes/metodos-de-instancia.js
+++ classes/metodos-de-instancia.js
@@ -15,3 +15,29 @@ let carro1 = new Carro('Gol', 'Volkswagen', 2020);
 let carro2 = new Carro('Civic', 'Honda', 2019);
 console.log(carro1.marcaEAno());
 console.log(carro2.marcaEAno());
+
+// Definição da classe
+class Usuario {
+  constructor(nome, sobrenome) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+  }
+
+  obterNomeCompleto() {
+    return 'Nome completo aqui';
+  }
+
+  podeVotar() {
+    return true;
+  }
+
+  jaVotou() {
+    return false;
+  }
+}
+
+// Uso da classe
+let usuario = new Usuario('Daniela', 'Queiroz');
+console.log(usuario.obterNomeCompleto());
+console.log(usuario.podeVotar());
+console.log(usuario.jaVotou());
