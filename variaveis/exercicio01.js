1402ef851214d79fe05a4a7a211a9009f6e3c5ca
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2025-10-24 20:58:11 -0300

    feat: adicionar exercícios de lógica para declaração de variáveis e uso de console.log

---
+++ variaveis/exercicio01.js
@@ -0,0 +1,4 @@
+let numberOfCookies = 2;
+
+const temperaturaDoForno = 180;
+console.log(temperaturaDoForno);
---
+++ variaveis/exercicio02.js
@@ -0,0 +1,14 @@
+// Bem-vindo à cozinha do código! Vamos assar alguns biscoitos de código.
+// A FAZER: Declare uma variável para o tipo de biscoitos que você está assando. Que tal biscoitos com gotas de chocolate?
+
+// A FAZER: Defina uma constante para a temperatura do forno. Dica: os biscoitos assam bem a 356 graus Fahrenheit.
+
+// A FAZER: Use console.log para anunciar o tipo de biscoitos e a temperatura de cozimento.
+
+let chocolateChipCookie = 'chocolate chips';
+const ovenTemperature = 356;
+const message = `
+  type of cookies: ${chocolateChipCookie}
+  oven temperature: ${ovenTemperature}°F
+`;
+console.log(message);
