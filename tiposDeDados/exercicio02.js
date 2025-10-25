f990b9c4068963e882b6e5e868210b412367f67e
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2025-10-24 21:59:45 -0300

    feat: adicionar exercício sobre tipos de dados em JavaScript

---
+++ tiposDeDados/exercicio02.js
@@ -0,0 +1,9 @@
+const numberOfPlanets = 8; // Number of planets in our solar system
+let isEarthOrbitRound = true; // Boolean indicating the shape of Earth's orbit
+const earthOrbit = 'elliptical'; // String representing Earth's orbit shape
+let undefinedOrbit; // Undefined variable for an unknown orbit
+
+console.log(typeof numberOfPlanets);
+console.log(typeof isEarthOrbitRound);
+console.log(typeof earthOrbit);
+console.log(typeof undefinedOrbit);
