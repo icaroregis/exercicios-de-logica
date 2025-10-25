b4afd8571280dea94689c521b330285fe6f28a8e
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2025-10-24 21:55:25 -0300

    refatorar: corrigir comentários sobre valores nulos e indefinidos em exercício de JavaScript

--- tiposDeDados/exercicio01.js
+++ tiposDeDados/exercicio01.js
@@ -2,7 +2,7 @@
 // undefined aparece quando você esquece de dar/atribuir um valor.
 // null aparece quando você diz que o valor é “nada”.
 
-let spaceOutsideUniverse = null;
-let firstHumanOnMars = undefined;
+let spaceOutsideUniverse = null; // valor intencionalmente definido como nulo
+let firstHumanOnMars; // ausência de valor atribuído, logo é undefined
 console.log(spaceOutsideUniverse);
 console.log(firstHumanOnMars);
