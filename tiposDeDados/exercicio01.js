daa2bfae84c316ea2f9dbcc49f0d121fc1fc36cb
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2025-10-24 21:53:52 -0300

    feat: adicionar exercício sobre null e undefined em JavaScript

---
+++ tiposDeDados/exercicio01.js
@@ -0,0 +1,8 @@
+// No JavaScript:
+// undefined aparece quando você esquece de dar/atribuir um valor.
+// null aparece quando você diz que o valor é “nada”.
+
+let spaceOutsideUniverse = null;
+let firstHumanOnMars = undefined;
+console.log(spaceOutsideUniverse);
+console.log(firstHumanOnMars);
