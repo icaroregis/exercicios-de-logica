410639fc7dc6077efd4a334ae6f5f7abf6c62139
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2025-10-24 22:12:04 -0300

    feat: adicionar exercícios sobre operadores de comparação em JavaScript

---
+++ operadores-de-comparacao/exercicio01.js
@@ -0,0 +1,8 @@
+// Igual a (==) , Diferente de (!=) - Consideram apenas o valor, desconsiderando o tipo.
+console.log(5 == '5'); // true, porque os valores são iguais
+console.log(5 != '5'); // false, porque os valores são iguais
+
+// Estritamente igual a (===) , Não estritamente igual a (!==)
+// Elas consideram tanto o valor quanto o tipo.
+console.log(5 === '5'); // false, porque o tipo é diferente
+console.log(5 !== '5'); // true, porque o tipo e o valor são diferentes
