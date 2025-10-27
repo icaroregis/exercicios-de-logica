fddd0647fad363fb88dd881213cf501d3c82e7f3
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2025-10-27 20:58:00 -0300

    feat: implementar lógica para verificar se é possível comprar quatro doces com o orçamento

---
+++ matematica/exercicio05.js
@@ -0,0 +1,12 @@
+// A FAZER: Declarar uma constante para o preço de um doce
+// A FAZER: Declarar uma constante para o seu orçamento total
+
+// A FAZER: Determinar se você pode comprar exatamente 4 doces com o seu orçamento e armazenar o resultado em uma variável
+
+// A FAZER: Usar console.log para imprimir se você pode comprar quatro doces
+
+const candy = 2;
+const budget = 10;
+const result = budget / candy >= 4 && budget / candy < 5;
+
+console.log(result);
