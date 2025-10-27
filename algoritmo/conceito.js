4cb21e4cffb6eb0e2de6c384ee04a437a38c1787
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2025-10-27 19:32:26 -0300

    feat: adicionar lógica para verificar se é possível comprar um sorvete e um doce com o orçamento

---
+++ algoritmo/conceito.js
@@ -0,0 +1,16 @@
+// ! O que é um algoritmo?
+// ! Um algoritmo é uma sequência finita de passos ou instruções bem definidas e ordenadas que são seguidas para resolver um problema específico ou realizar uma tarefa. Eles são fundamentais na programação de computadores, pois fornecem um método sistemático para processar dados e executar operações.
+
+// ? Características principais de um algoritmo:
+// ? 1. Finitude: Um algoritmo deve sempre terminar após um número finito de etapas.
+// ? 2. Definição clara: Cada passo do algoritmo deve ser claramente definido e não ambíguo.
+// ? 3. Entrada: Um algoritmo pode ter zero ou mais entradas, que são os dados fornecidos para processamento.
+// ? 4. Saída: Um algoritmo deve produzir pelo menos uma saída, que é o resultado do processamento das entradas.
+// ? 5. Efetividade: As operações realizadas em cada passo do algoritmo devem ser suficientemente básicas para serem executadas em um tempo finito.
+
+// ? Exemplos de algoritmos no dia a dia:
+// ? - Receita de culinária: Seguir uma receita para preparar um prato específico.
+// ? - Instruções de montagem: Passos para montar um móvel ou equipamento.
+// ? - Algoritmos de busca: Métodos para encontrar informações em bancos de dados ou na internet.
+
+// ? Em resumo, algoritmos são essenciais para a resolução de problemas e automação de tarefas em diversas áreas, especialmente na ciência da computação.
---
+++ matematica/exercicio04.js
@@ -0,0 +1,9 @@
+// Bem-vindo à loja de doces!
+const preçoDoDoce = 2;
+const preçoDoSorvete = 4;
+const orçamento = 10;
+
+// A FAZER: Posso comprar um sorvete e um doce com um orçamento de $10? Use operadores aritméticos e lógicos para decidir.
+const possoComprarSorveteEDoce = orçamento >= preçoDoSorvete + preçoDoDoce;
+
+console.log('Posso comprar um sorvete e um doce com meu orçamento de $10?', possoComprarSorveteEDoce);
