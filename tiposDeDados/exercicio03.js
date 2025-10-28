4379e32ed3f44d46c055ee80bc9e3f68ad168c8f
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2025-10-27 21:58:23 -0300

    feat: implementar lógica para converter cordas em números e somar detritos espaciais

---
+++ tiposDeDados/exercicio03.js
@@ -0,0 +1,9 @@
+// Convertendo cordas cósmicas em números e somando-as
+let cometTail = '42';
+let nebulaDust = '23';
+let asteroidBelt = '16';
+
+// A FAZER: Converter as variáveis de cordas cósmicas em números e somá-las para encontrar o total de detritos espaciais
+const totalSpaceDebris = (+cometTail) + (+nebulaDust) + (+asteroidBelt);
+// A FAZER: Imprimir o total de detritos espaciais no console
+console.log(totalSpaceDebris);
