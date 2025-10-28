ab9400ad3b3a9b90ab9b6f0a4f8f0c39d2029983
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2025-10-27 23:00:17 -0300

    feat: adicionar exercício de condicionais com operador ternário

---
+++ condicionais/exercicio01.js
@@ -0,0 +1,10 @@
+// Ótimo trabalho, Stellar Navigator! É hora de um pequeno desafio. Você pode alterar a instrução if fornecida para um operador ternário?
+
+// Você consegue, deixe ir!
+
+const weather = 'rainy';
+const temperature = 21;
+
+weather === 'rainy' && temperature > 20
+  ? console.log('How about a board game indoors?')
+  : console.log("Let's have a picnic!");
