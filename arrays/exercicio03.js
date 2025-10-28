b6774b65ba8f303b71a05063206798d950e67e2b
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2025-10-28 00:37:11 -0300

    feat: ajustar código para encontrar o último planeta no aglomerado

---
+++ arrays/exercicio03.js
@@ -0,0 +1,8 @@
+// Navegador Estelar, sua próxima missão é ajustar o código do nosso banco de dados de Exploração Espacial. Em vez de descobrir a primeira estrela, vamos encontrar o nome do último planeta no aglomerado "Marte, Júpiter, Saturno". Use seu novo conhecimento de indexação de listas aninhadas para concluir esta tarefa.
+
+const planets = [
+  ['Mercury', 'Venus', 'Earth'],
+  ['Mars', 'Jupiter', 'Saturn'],
+];
+
+console.log(planets[1][2]);
