a813f89a24ea845624eb4cfa38342c7c03a3b605
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2025-10-31 15:07:42 -0300

    feat: adicionar exercícios com conjuntos e manipulação de elementos celestiais

---
+++ arrays/exercicio10.js
@@ -0,0 +1,3 @@
+// TODO: Create a new Set containing 'Meteorite', 'Asteroid', 'Comet', and another 'Asteroid'. Log the Set to the console.
+const collection = new Set(['Meteorite', 'Asteroid', 'Comet', 'Asteroid']);
+console.log(collection); // The Set should only contain unique elements
