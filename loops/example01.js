f0649a31abd1206b81c0d79a25f5a65ea45f431f
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2025-10-31 23:05:27 -0300

    feat: adicionar exemplo de laços while aninhados em JavaScript

--- loops/example01.js
+++ loops/example01.js
@@ -15,3 +15,25 @@ i = 2 , j = 0
 i = 2 , j = 1
 i = 2 , j = 2
 */
+
+let i = 0;
+while (i < 3) {
+  let j = 0;
+  while (j < 3) {
+    console.log('i =', i, ', j =', j);
+    j++;
+  }
+  i++;
+}
+/*
+Prints:
+i = 0 , j = 0
+i = 0 , j = 1
+i = 0 , j = 2
+i = 1 , j = 0
+i = 1 , j = 1
+i = 1 , j = 2
+i = 2 , j = 0
+i = 2 , j = 1
+i = 2 , j = 2
+*/
