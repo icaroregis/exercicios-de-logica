9e75d4f821586624b5c3b9b961b5740af448d6b0
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2025-11-04 20:24:06 -0300

    feat: adicionar mensagem de lançamento e prévia de mensagem para a missão

--- strings/exercicio04.js
+++ strings/exercicio04.js
@@ -2,9 +2,9 @@ let spacecraft = 'Discovery';
 let mission = 'Jupiter orbit';
 let launchDate = 'August 29, 2040';
 
-let launchMessage = ___;
+let launchMessage = `${spacecraft} is set to embark on a mission to ${mission} on ${launchDate}.`;
 
-let messagePreview = ___;
+let messagePreview = launchMessage.substring(0, 25).concat('...');
 
 console.log(launchMessage.toUpperCase());
 console.log('Preview: ' + messagePreview);
