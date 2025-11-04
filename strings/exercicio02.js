d12052af9833abd5dfcb89b513114f45eb2446db
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2025-11-04 14:54:49 -0300

    feat: otimizar mensagens de embarque com uso de toLocaleUpperCase

--- strings/exercicio02.js
+++ strings/exercicio02.js
@@ -1,8 +1,7 @@
-let spaceshipName = 'Starship';
-let destination = 'Mars';
+let spaceshipName = 'Starship'.toLocaleUpperCase();
+let destination = 'Mars'.toLocaleUpperCase();
 let boardingMessage = spaceshipName + ' boarding for ' + destination + ' starts now!';
 let missionStatus = boardingMessage.substring(9, 17) + ' ' + 'is ready for liftoff!';
 
-console.log(boardingMessage.toLocaleUpperCase()); // Should print: STARSHIP boarding for MARS starts now!
+console.log(boardingMessage); // Should print: STARSHIP boarding for MARS starts now!
 console.log(missionStatus.toLocaleLowerCase()); // Should print: boarding is ready for liftoff!
-console.log(spaceshipName.indexOf('ship')); // Should print: 4
