a673c1c63f5e48c1963a13517ba9c748b20bc691
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2025-11-06 23:02:10 -0300

    feat: adicionar exemplos de uso de caracteres de escape em strings

---
+++ escapeCharacter/exercicio03.js
@@ -0,0 +1,2 @@
+let favoriteBook = "Alice's Adventures in Wonderland by \"Lewis Carrol\" ";
+console.log("He exclaimed, \"That's my favorite book too!\" when he saw her reading " + favoriteBook + ".");
---
+++ escapeCharacter/exercicio04.js
@@ -0,0 +1,3 @@
+const bookTitle = 'The War of the Worlds: "A Science Fiction Classic"';
+const dialogue = 'He said, "It\'s a matter of life and death.", and took a deep breath.';
+console.log(`${bookTitle} ${dialogue}`);
