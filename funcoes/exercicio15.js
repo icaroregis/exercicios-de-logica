ea8d7728898cfe1cd2340e73e8a04b2ea71560bf
icaroregis <progalmeidaregis@outlook.com>
2025-11-09 17:40:15 -0300

    refactor: remover variável desnecessária na função logAstronomicalEvents

--- funcoes/exercicio15.js
+++ funcoes/exercicio15.js
@@ -1,7 +1,6 @@
 function logAstronomicalEvents(...events) {
   // * Aqui events é um array que contém todos os argumentos passados para a função
-  const restEvents = events;
-  for (let event of restEvents) {
+  for (let event of events) {
     console.log(`A cosmic event is happening: ${event}!`);
   }
 }
