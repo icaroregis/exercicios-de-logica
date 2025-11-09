eabe2f78e6c5a9a04cff29ecb2e70cce351af26a
icaroregis <progalmeidaregis@outlook.com>
2025-11-09 17:42:07 -0300

    fix: corrigir uso da variável starNames na função logStarsInfo

--- funcoes/exercicio14.js
+++ funcoes/exercicio14.js
@@ -1,7 +1,7 @@
 function logStarsInfo(...starNames) {
-  const restStars = starNames;
+  // * Aqui starNames é um array que contém todos os argumentos passados para a função
   for (let star = 0; star < starNames.length; star++) {
-    console.log(`Star ${star + 1}: ${restStars[star]} is part of our galaxy.`);
+    console.log(`Star ${star + 1}: ${starNames[star]} is part of our galaxy.`);
   }
 }
 
