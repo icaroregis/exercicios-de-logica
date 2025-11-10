ddc6314aa3934584c3d1d98efa38e760e704c532
icaroregis <progalmeidaregis@outlook.com>
2025-11-10 19:18:44 -0300

    feat: adicionar classe Fruit e exemplos de instância em JavaScript

---
+++ classes/classes-criacao-de-objetos.js
@@ -0,0 +1,11 @@
+// * Imagine uma classe JavaScript como uma planta de construção. Seguindo essa planta, podemos criar objetos com estruturas específicas, cada um preenchido com valores diferentes.
+
+class Fruit {}
+
+// * Aqui Fruitestá o nosso modelo ou classe. Esse modelo nos permite gerar uma variedade de objetos de frutas com atributos especializados, de forma semelhante à construção de um edifício.
+
+// * Criar uma instância de uma classe, essencialmente dar vida a um objeto a partir desse modelo, usa a new palavra-chave:
+
+let apple = new Fruit();
+
+// * Neste exemplo, apple trata-se de uma instância ou objeto específico da nossa Fruit classe, semelhante a um edifício construído a partir de uma planta compartilhada.
