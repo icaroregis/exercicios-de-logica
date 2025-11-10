520c2b424fc9a566d68cdca89af49e8f081149d1
icaroregis <progalmeidaregis@outlook.com>
2025-11-10 16:28:57 -0300

    fix: corrigir lançamento de erro na função checkOxygenLevel para usar Error

--- funcoes/exercicio19.js
+++ funcoes/exercicio19.js
@@ -1,12 +1,12 @@
 function checkOxygenLevel(level) {
   if (level < 90) {
-    throw 'Danger! Oxygen level is too low!';
+    throw new Error('Danger! Oxygen level is too low!');
   }
   console.log('Oxygen level is stable.');
 }
 
 try {
   checkOxygenLevel(85);
-} catch (error) {
-  throw error;
+} catch (err) {
+  console.log(err.message);
 }
