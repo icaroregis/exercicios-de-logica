ca3c2b5d12afdc9435209fe1c836875403c1be52
icaroregis <progalmeidaregis@outlook.com>
2025-11-10 22:02:05 -0300

    feat: implementar classe Vehicle com controle de nível de combustível e atributos privados

---
+++ classes/exercicio18.js
@@ -0,0 +1,32 @@
+class Vehicle {
+  constructor(name) {
+    this._name = name; // Private attribute _name
+    this._fuelLevel = 100; // Private attribute _fuelLevel
+  }
+
+  get fuelLevel() {
+    return this._fuelLevel;
+  }
+
+  set fuelLevel(level) {
+    if (typeof level !== 'number' || level < 0 || level > 100) {
+      console.log('Invalid value! Please enter a value between 0 and 100.');
+      return;
+    }
+    if (this._fuelLevel < 0) this._fuelLevel = 0;
+    this._fuelLevel = level;
+  }
+
+  get nameVehicle() {
+    return this._name;
+  }
+}
+
+let myVehicle = new Vehicle('Cybertruck');
+myVehicle.fuelLevel = 90;
+
+console.log(myVehicle.fuelLevel);
+console.log(myVehicle.nameVehicle);
+
+myVehicle.fuelLevel = 50;
+console.log(myVehicle.fuelLevel);
