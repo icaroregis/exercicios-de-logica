f571a7658cc943c6f2985f847aa1dbb7202f7751
icaroregis <progalmeidaregis@outlook.com>
2025-11-10 22:26:30 -0300

    feat: corrigir nomenclatura de getters e setters na classe Vehicle

--- classes/exercicio19.js
+++ classes/exercicio19.js
@@ -1,26 +1,27 @@
+//! Getters e Setters devem terem a mesma nomenclatura.
 class Vehicle {
   constructor(vehicleName) {
     this._vehicleName = vehicleName;
     this._fuelLevel = 100;
   }
 
-  set refuel(level = 100) {
+  set fuelLevel(level = 100) {
     return (this._fuelLevel = level);
   }
 
-  get currentFuelLevel() {
+  get fuelLevel() {
     return this._fuelLevel;
   }
 
-  set updateFuelLevel(level) {
+  set fuelLevel(level) {
     return (this._fuelLevel = level);
   }
 }
 
 const orion = new Vehicle();
 
-orion.refuel = 100;
+orion.fuelLevel = 100;
 console.log(orion.currentFuelLevel);
 
-orion.updateFuelLevel = 50;
+orion.fuelLevel = 50;
 console.log(orion.currentFuelLevel);
