2df5a7369177ba5b7102c6ff0016c8baee164b81
icaroregis <progalmeidaregis@outlook.com>
2025-11-14 00:27:10 -0300

    feat: implementar função grantABenefit para cálculo de renda per capita e elegibilidade a benefícios

---
+++ dataEntry/src/exercicio20.js
@@ -0,0 +1,22 @@
+import readline from 'readline-sync';
+
+function grantABenefit(numberOfPeoples) {
+  const peopleList = [];
+  for (let people = 0; people < numberOfPeoples; people++) {
+    const monthlyIncome = parseFloat(readline.question(`Qual a renda mensal da ${people + 1} pessoa: `));
+    peopleList.push(monthlyIncome);
+  }
+  const result = peopleList.reduce((accumulator, currentValue) => accumulator + currentValue, 0) / numberOfPeoples;
+
+  if (result >= 1500) {
+    console.log(
+      `A renda per capita da sua família é: ${result.toFixed(
+        2,
+      )}.\nInfelizmente você não está opto(a) para receber o benefício!`,
+    );
+  } else {
+    console.log(`A renda per capita da sua família é: ${result.toFixed(2)}.\nParabéns o benefício pode ser concedido!`);
+  }
+}
+const numberOfPeoples = Number(readline.question('Quantas pessoas no total residem em sua casa? '));
+grantABenefit(numberOfPeoples);
