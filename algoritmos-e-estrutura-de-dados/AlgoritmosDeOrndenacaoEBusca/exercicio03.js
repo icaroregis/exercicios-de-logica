209faeb42dc592f54736b1ef873895cbe3446cac
icaroregis <progalmeidaregis@outlook.com>
2025-11-16 18:54:59 -0300

    feat: corrigir função fatorial e manter contagem de camadas de bolo

--- algoritmos-e-estrutura-de-dados/AlgoritmosDeOrndenacaoEBusca/exercicio03.js
+++ algoritmos-e-estrutura-de-dados/AlgoritmosDeOrndenacaoEBusca/exercicio03.js
@@ -1,10 +1,20 @@
-function factorial(n) {
-  if (n === 1) {
-    console.log(n);
-    return n;
+// Você consegue identificar o que está errado com nossa função fatorial? Examine o código e corrija o erro para calcular o fatorial de um número com precisão.
+
+function countCakeLayers(layers) {
+  if (layers === 1) {
+    return 1; // Only one layer
   } else {
-    console.log(n);
-    return n * factorial(n - 1);
+    return 1 + countCakeLayers(layers - 1); // Count this layer plus the rest
   }
 }
-console.log(factorial(5)); // Should print 120
+
+console.log(countCakeLayers(5)); // Should print 5
+
+// 1 + (5 - 1 = 4)
+// 1 + (4 - 1 = 3)
+// 1 + (3 - 1 = 2)
+// 1 + (2 - 1 = 1)
+// 1 + 1
+
+// agora quando chega na condição de parada voltamos somando os valores de return
+// 1 + 1 + 1 + 1 + 1 = 5
