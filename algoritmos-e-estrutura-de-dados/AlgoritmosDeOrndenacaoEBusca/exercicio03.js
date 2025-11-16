f33ac763de753cf8741509ac78424202253913bd
icaroregis <progalmeidaregis@outlook.com>
2025-11-16 19:52:15 -0300

    feat: adicionar explicações detalhadas sobre o cálculo do fatorial na função de contagem de camadas de bolo

--- algoritmos-e-estrutura-de-dados/AlgoritmosDeOrndenacaoEBusca/exercicio03.js
+++ algoritmos-e-estrutura-de-dados/AlgoritmosDeOrndenacaoEBusca/exercicio03.js
@@ -8,6 +8,7 @@ function countCakeLayers(layers) {
   }
 }
 console.log(countCakeLayers(5)); // Should print 5
+
 //!A multiplicação de layers * (5 - 1 = 4) nunca acontece pois a cada loop a função é chamada novamente
 
 // 1 loop: 5 * (5 - 1 = 4)
@@ -18,3 +19,8 @@ console.log(countCakeLayers(5)); // Should print 5
 
 // agora quando chega na condição de parada voltamos somando os valores de return
 // 5loop: 1 * 4loop: 2 * 3loop: 3 * 2loop: 4 * 1loop: 5 = 120
+
+// countCakeLayers(2) retorna 2 * 1 = 2
+// countCakeLayers(3) retorna 3 * 2 = 6
+// countCakeLayers(4) retorna 4 * 6 = 24
+// countCakeLayers(5) retorna 5 * 24 = 120
