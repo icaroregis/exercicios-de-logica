78c360ec5fb5ea9862ce10d021d6d220c595c085
icaroregis <progalmeidaregis@outlook.com>
2025-11-20 00:06:40 -0300

    feat: coletar informações sobre alergias do paciente

--- dataEntry/src/exercicio26.js
+++ dataEntry/src/exercicio26.js
@@ -12,10 +12,35 @@ const imcMessages = [
 const patientInformation = {};
 const description = 'do paciente';
 
+function validationLetter(world) {
+  return world.trim() === 's';
+}
+
+function changeToUppercase(description) {
+  const newString = description.slice(3, description.length);
+  const result = newString[0].toUpperCase() + newString.slice(1);
+  return result;
+}
+
 patientInformation.name = readline.question(`Qual o nome ${description}? `);
 patientInformation.age = parseFloat(readline.question(`Qual o idade ${description}? `));
 patientInformation.weight = parseFloat(readline.question(`Qual o peso ${description}? `));
 patientInformation.height = parseFloat(readline.question(`Qual a altura ${description}? `));
+const seafood = readline.question(`${changeToUppercase(description)} possui alergia a frutos do mar? (s/n)`);
+const bees = readline.question(`${changeToUppercase(description)} possui alergia a abelhas? (s/n)`);
+const animalHair = readline.question(`${changeToUppercase(description)} possui alergia a pelos de animais? (s/n)`);
+const pollen = readline.question(`${changeToUppercase(description)} possui alergia a pólen? (s/n)`);
+const lactose = readline.question(`${changeToUppercase(description)} possui alergia a lactose? (s/n)`);
+
+patientInformation.seafoodAllergy = {
+  seafood: validationLetter(seafood),
+  bees: validationLetter(bees),
+  animalHair: validationLetter(animalHair),
+  pollen: validationLetter(pollen),
+  lactose: validationLetter(lactose),
+};
+
+console.log(patientInformation.seafoodAllergy);
 
 function calcImc(imcList, infoDataPatient) {
   const imc = infoDataPatient.weight / (infoDataPatient.height * infoDataPatient.height);
